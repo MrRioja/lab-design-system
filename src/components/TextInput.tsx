38e73e79448a730c6aa7231afb40c8681b61cf83
Luiz Rioja <lulyrioja@gmail.com>
2022-11-12 22:40:09 -0300

    feat(TextInput): create, style and document the component

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,48 @@
+import { Slot } from "@radix-ui/react-slot";
+import { InputHTMLAttributes, ReactNode } from "react";
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot({ children }: TextInputRootProps) {
+  return (
+    <div className="flex items-center h-12 gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+      {children}
+    </div>
+  );
+}
+
+TextInputRoot.displayName = "TextInput.Root";
+
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon({ children }: TextInputIconProps) {
+  return <Slot className="w-6 h-6 text-gray-400">{children}</Slot>;
+}
+
+TextInputIcon.displayName = "TextInput.Icon";
+
+export interface TextInputInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {}
+
+function TextInputInput(props: TextInputInputProps) {
+  return (
+    <input
+      {...props}
+      className={
+        "bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      }
+    />
+  );
+}
+
+TextInputInput.displayName = "TextInput.Input";
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+};
