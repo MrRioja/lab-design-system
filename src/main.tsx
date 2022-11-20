5ae7826fba2efb83932b77a7dd6fed6b51110461
Luiz Rioja <lulyrioja@gmail.com>
2022-11-20 19:33:48 -0300

    fix: remove unnecessary import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
-import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
