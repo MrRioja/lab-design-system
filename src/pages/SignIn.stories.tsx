98be4c3c76db20c285ea057ee305a260b5a50c27
Luiz Rioja <lulyrioja@gmail.com>
2022-11-25 21:53:35 -0300

    feat(SignIn): create stories of page

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { SignIn } from "./SignIn";
+
+export default {
+  title: "Components/Sign In",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {};
